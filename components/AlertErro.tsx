6bac52489c2c5658151769ca9cc3488fba320a07
Eduardo Pedrosa <eduardobaptista66@gmail.com>
2025-02-20 12:41:22 -0300

    animando o componente de alerta com a framer-motion

--- components/AlertErro.tsx
+++ components/AlertErro.tsx
@@ -1,6 +1,7 @@
 import { XCircle } from "lucide-react";
 import { useEffect, useState } from "react";
 import { FaTriangleExclamation } from "react-icons/fa6";
+import { motion, AnimatePresence } from "framer-motion";
 
 export default function AlertErro({
   message,
@@ -16,24 +17,31 @@ export default function AlertErro({
   useEffect(() => {
     setIsOpen(open);
   }, [open]);
+
   return (
-    <>
+    <AnimatePresence>
       {isOpen && (
-        <div className="bg-red-600 my-5 text-gray-50 p-3 rounded-lg flex items-start justify-between shadow-md">
+        <motion.div
+          initial={{ opacity: 0, y: -10 }}
+          animate={{ opacity: 1, y: 0 }}
+          exit={{ opacity: 0, y: -10 }}
+          transition={{ duration: 0.3, ease: "easeInOut" }}
+          className="bg-red-600 my-5 text-gray-50 p-3 rounded-lg flex items-start justify-between shadow-md"
+        >
           <p className="grid grid-cols-[20px_1fr] items-center gap-2 text-base">
             <FaTriangleExclamation className="text-xl" />
             {message}
           </p>
           <button
-            onClick={() => onClose()}
+            onClick={onClose}
             type="button"
             title="fechar alerta"
             className="ml-5"
           >
             <XCircle className="w-5 h-5" />
           </button>
-        </div>
+        </motion.div>
       )}
-    </>
+    </AnimatePresence>
   );
 }
