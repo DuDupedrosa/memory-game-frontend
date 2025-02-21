16e88e604a15840ead04f863ac34ec97e62c7549
Eduardo Pedrosa <eduardobaptista66@gmail.com>
2025-02-21 11:50:14 -0300

    socket env url

--- app/socket.ts
+++ app/socket.ts
@@ -2,7 +2,7 @@
 
 import { io } from "socket.io-client";
 
-export const socket = io("http://localhost:3000", {
+export const socket = io(process.env.NEXT_PUBLIC_SOCKET_URL ?? "", {
   withCredentials: true, // Permite o envio de cookies e credenciais
   transports: ["websocket"], // Força o uso do WebSocket (se possível)
 });
